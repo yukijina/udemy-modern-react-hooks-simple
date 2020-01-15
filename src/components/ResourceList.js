e1d20a9e4d38f612357ce74df4f821afaca552e1
yukijina <yukijina@gmail.com>
2020-01-15 12:15:44 -0800

    create file - child of App - display posts and todos

---
+++ src/components/ResourceList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ResourceList extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.resource}
+            </div>
+        )
+    }
+}
+export default ResourceList;
