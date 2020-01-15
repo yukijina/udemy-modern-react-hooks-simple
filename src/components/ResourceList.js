393f840456b0b5c39b9204d85c11f6facf8d101a
yukijina <yukijina@gmail.com>
2020-01-15 14:09:28 -0800

    refactor useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,20 +4,38 @@ import axios from 'axios'
 const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([])
 
-    const fetchResource = async () => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+    // const fetchResource = async () => {
+    //     const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
 
-        setResources(response.data)
-    }
+    //     setResources(response.data)
+    // }
 
-    useEffect(() => {
-        fetchResource(resource)
+    // useEffect(() => {
+    //     fetchResource(resource)
+    // }, [resource])
+    // second array "resouce" is from App.js "props.resouce || { resource }"
+    // No array means called only once
+    // But if you want to use as componentDidUpdate function, you want to invoke only once. Put empty array [] otherwise it called forever(loop)
+    
+    // Another way
+    useEffect(
+        () => {
+            (async resource => {
+                const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+        
+                setResources(response.data)
+            })(resource); // invoking right way - same as (()=> somecode)()
     }, [resource])
-    // second argument "resouce" is from App.js "props.resouce || { resource }"
+
+
+
 
         return (
             <div>
-                {resources.length}
+                <ul>{resources.map(record => (
+                    <li key={record.id}>{record.title}</li>
+                ))}
+                </ul>
             </div>
         )
     
