1e4cad6fc67c255d318ae974f9512571cf6aee56
yukijina <yukijina@gmail.com>
2020-01-15 14:49:36 -0800

    refactor - move async to useResources file

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,22 +1,7 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios'
-
-// N0.3 another way - reusable
-const useResources = (resource) => {
-    const [resources, setResources] = useState([])
-
-    useEffect(
-        () => {
-            (async resource => {
-                const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-        
-                setResources(response.data)
-            })(resource); // invoking right way - same as (()=> somecode)()
-    }, [resource])
-
-    // return array
-    return resources
-}
+import React from 'react';
+import useResources from './useResources'
+//import React, { useState, useEffect} from 'react';
+//import axios from 'axios'
 
 const ResourceList = ({ resource }) => {
     // const [resources, setResources] = useState([])
